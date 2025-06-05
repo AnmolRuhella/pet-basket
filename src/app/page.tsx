5a7bd0513bcb067587aa7ea8c3f7607e022fcb45
Anmol9586 <anmolruhella6@gmail.com>
2025-06-06 00:52:20 +0530

    setup project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function page() {
+function Home() {
   return (
-    <div>Welecome to pet market</div>
+    <div>Home page</div>
   )
 }
 
-export default page
+export default Home;
